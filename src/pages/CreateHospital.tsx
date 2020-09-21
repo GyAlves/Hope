923ff4633bb40bad9a6761e86a79a81811fd51b4
GyAlves <gyasminassuncao@uni9.edu.br>
2020-09-21 02:10:53 -0300

    Project finished

--- src/pages/CreateHospital.tsx
+++ src/pages/CreateHospital.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { useHistory } from 'react-router-dom';
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
+import {LeafletMouseEvent} from 'leaflet';
 import {  FiPlus } from "react-icons/fi";
 
 import Sidebar from '../components/Sidebar';
 
 import mapMarkerImg from '../assets/target.svg';
 import '../styles/pages/create-hospital.css';
+import api from "../services/api";
 
 const hopeMapIcon = L.icon({
   iconUrl: mapMarkerImg,
@@ -17,47 +20,113 @@ const hopeMapIcon = L.icon({
 })
 
 export default function CreateHospital() {
+  const history = useHistory();
+  const [position, setPosition] = useState({latitude:0, longitude:0});
+
+  const [name, setName] = useState('');
+  const [about, setAbout] = useState('');
+  const [instructions, setInstructions] = useState('');
+  const [open_hours, setOpenHours] = useState('');
+  const [open_to_visitation, setOpenToVisitation] = useState(true); 
+  const [images, setImages] = useState<File[]>([]);
+  const [previewImages, setPreviewImages] = useState<string[]>([])
+
+
+  function handleMapClick(event: LeafletMouseEvent){
+    const {lat, lng} = event.latlng;
+
+    setPosition({
+      latitude: lat,
+      longitude: lng
+    })
+  }
+
+  async function handleSubmit(event: FormEvent){
+    event.preventDefault();
+
+    const {latitude, longitude} = position;
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('latitude', String(latitude));
+    data.append('name', String(longitude));
+    data.append('name', name);
+    data.append('about', about);
+    data.append('instructions', instructions);
+    data.append('open_hours', open_hours);
+    data.append('open_to_visitation', String(open_to_visitation));
+    
+    images.forEach(image => {
+      data.append('images', image);
+    })
+
+    await api.post('/hospitals',data);
+    alert('Cadastro realizado com sucesso')
+    history.push('/app')
+  }
+
+  function handleSelectImages(event: ChangeEvent<HTMLInputElement>){
+    if(!event.target.files){
+      return;
+    }
+
+    const selectedImages = Array.from(event.target.files);
+    setImages(selectedImages);
+
+    const selectedImagesPreview = selectedImages.map(image => {
+      return URL.createObjectURL(image)
+    })
+
+    setPreviewImages(selectedImagesPreview);
+  }
   return (
     <div id="page-create-orphanage">
       <Sidebar/>
 
       <main>
-        <form className="create-orphanage-form">
+        <form onClick={handleSubmit} className="create-orphanage-form">
           <fieldset>
             <legend>Dados</legend>
 
             <Map 
-              center={[-27.2092052,-49.6401092]} 
+              center={[-23.4422272,-46.5272832]} 
               style={{ width: '100%', height: 280 }}
               zoom={15}
+              onclick={handleMapClick}
             >
               <TileLayer 
                 url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
 
-              <Marker interactive={false} icon={hopeMapIcon} position={[-27.2092052,-49.6401092]} />
+              {position.latitude !== 0 
+                && <Marker interactive={false} icon={hopeMapIcon} position={[position.latitude,position.longitude]} />
+              }
             </Map>
 
             <div className="input-block">
               <label htmlFor="name">Nome</label>
-              <input id="name" />
+              <input id="name" value={name} onChange={event => setName(event.target.value)}/>
             </div>
 
             <div className="input-block">
               <label htmlFor="about">Sobre <span>Máximo de 300 caracteres</span></label>
-              <textarea id="name" maxLength={300} />
+              <textarea id="name" maxLength={300} value={about} onChange={event => setAbout(event.target.value)}/>
             </div>
 
             <div className="input-block">
               <label htmlFor="images">Fotos</label>
 
-              <div className="uploaded-image">
-
+              <div className="images-container">
+                {previewImages.map(image => {
+                  return(
+                    <img key={image} src={image} alt={name} />
+                  )
+                })}
+                <label htmlFor="image[]" className="new-image">
+                  <FiPlus size={24} color="#15b6d6" />
+                </label>
               </div>
-
-              <button className="new-image">
-                <FiPlus size={24} color="#15b6d6" />
-              </button>
+              <input multiple onChange={handleSelectImages} type="file" id="image[]"/>
             </div>
           </fieldset>
 
@@ -66,20 +135,32 @@ export default function CreateHospital() {
 
             <div className="input-block">
               <label htmlFor="instructions">Instruções</label>
-              <textarea id="instructions" />
+              <textarea id="instructions" value={instructions} onChange={event => setInstructions(event.target.value)}/>
             </div>
 
             <div className="input-block">
-              <label htmlFor="opening_hours">Nome</label>
-              <input id="opening_hours" />
+              <label htmlFor="opening_hours">Horário de funcionamento</label>
+              <input id="opening_hours" value={open_hours} onChange={event => setOpenHours(event.target.value)} />
             </div>
 
             <div className="input-block">
               <label htmlFor="open_on_weekends">Aberto para visitas</label>
 
               <div className="button-select">
-                <button type="button" className="active">Sim</button>
-                <button type="button">Não</button>
+                <button 
+                  type="button" 
+                  className={open_to_visitation ? 'active': ''}
+                  onClick={() => setOpenToVisitation(true)}
+                >
+                  Sim
+                </button>
+                <button 
+                  type="button"
+                  className={!open_to_visitation? 'active': ''}
+                  onClick={() => setOpenToVisitation(false)}
+                >
+                  Não
+                </button>
               </div>
             </div>
           </fieldset>
